80ff6b7a9a24aba375c8096e10382241c3ecdb21
NAZMI <NAZMI@DESKTOP-20STISF>
2018-12-09 18:55:02 +0800

    ToSnakeCase method added for postgres db

--- src/TestOkur.Infrastructure/Extensions/ModelBuilderExtensions.cs
+++ src/TestOkur.Infrastructure/Extensions/ModelBuilderExtensions.cs
@@ -7,6 +7,38 @@
 
     public static class ModelBuilderExtensions
     {
+        public static void ToSnakeCase(this ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                // Replace table names
+                entity.Relational().TableName = entity.Relational().TableName.ToSnakeCase();
+
+                // Replace column names
+                foreach (var property in entity.GetProperties())
+                {
+                    property.Relational().ColumnName = property.Relational()
+                        .ColumnName
+                        .ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.Relational().Name = index.Relational().Name.ToSnakeCase();
+                }
+            }
+        }
+
         public static void ApplyAllConfigurationsFromCurrentAssembly(this ModelBuilder modelBuilder, string configNamespace = "")
         {
             var applyMethod = GetApplyConfigurationMethod();
