ad2de54d5efac2832aa8039c305d7760df9f3e8b
NAZMI <NAZMI@DESKTOP-20STISF>
2019-01-13 21:18:28 +0800

    QueryBase abstraction reworked

--- src/TestOkur.Infrastructure/Cqrs/QueryBase.cs
+++ src/TestOkur.Infrastructure/Cqrs/QueryBase.cs
@@ -1,7 +1,15 @@
 ﻿namespace TestOkur.Infrastructure.Cqrs
 {
+	using System.Diagnostics.CodeAnalysis;
+	using Paramore.Darker;
+
 	public abstract class QueryBase
-    {
-        public int UserId { get; set; }
-    }
+	{
+		public int UserId { get; set; }
+	}
+
+	[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleType", Justification = "Reviewed.")]
+	public abstract class QueryBase<TResult> : QueryBase, IQuery<TResult>
+	{
+	}
 }
