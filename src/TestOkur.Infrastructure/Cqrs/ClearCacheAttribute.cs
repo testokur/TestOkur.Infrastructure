593bc4da81ab9b3b463713a6f800093c914a0213
NAZMI <NAZMI@DESKTOP-20STISF>
2019-02-06 21:16:37 +0300

    Clear cache should be executed after the main request has been executed

--- src/TestOkur.Infrastructure/Cqrs/ClearCacheAttribute.cs
+++ src/TestOkur.Infrastructure/Cqrs/ClearCacheAttribute.cs
@@ -1,16 +1,16 @@
 ﻿namespace TestOkur.Infrastructure.Cqrs
 {
-    using System;
-    using Paramore.Brighter;
+	using System;
+	using Paramore.Brighter;
 
-    [AttributeUsage(AttributeTargets.Method)]
-    public sealed class ClearCacheAttribute : RequestHandlerAttribute
-    {
-        public ClearCacheAttribute(int step)
-            : base(step)
-        {
-        }
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class ClearCacheAttribute : RequestHandlerAttribute
+	{
+		public ClearCacheAttribute(int step)
+			: base(step, HandlerTiming.After)
+		{
+		}
 
-        public override Type GetHandlerType() => typeof(ClearCacheDecorator<>);
-    }
+		public override Type GetHandlerType() => typeof(ClearCacheDecorator<>);
+	}
 }
