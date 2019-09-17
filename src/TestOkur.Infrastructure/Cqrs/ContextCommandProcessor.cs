46b781b79394adbbc80ac92e8f6118a6ff678eba
Nazmi Altun <nazmialtun@windowslive.com>
2019-09-17 18:46:04 +0300

    Rework : HttpContext access from commandhandler

---
+++ src/TestOkur.Infrastructure/Cqrs/ContextCommandProcessor.cs
@@ -0,0 +1,24 @@
+﻿namespace TestOkur.Infrastructure.Cqrs
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Paramore.Brighter;
+
+    public class ContextCommandProcessor : IContextCommandProcessor
+    {
+        private readonly IAmACommandProcessor _commandProcessor;
+        private readonly IUserIdProvider _userIdProvider;
+
+        public ContextCommandProcessor(IAmACommandProcessor commandProcessor, IUserIdProvider userIdProvider)
+        {
+            _commandProcessor = commandProcessor;
+            _userIdProvider = userIdProvider;
+        }
+
+        public async Task ExecuteAsync<TCommand>(TCommand command, CancellationToken cancellationToken = default) where TCommand : CommandBase
+        {
+            command.UserId = await _userIdProvider.GetAsync();
+            await _commandProcessor.SendAsync(command, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ src/TestOkur.Infrastructure/Cqrs/IContextCommandProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace TestOkur.Infrastructure.Cqrs
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IContextCommandProcessor
+    {
+        Task ExecuteAsync<TCommand>(TCommand command, CancellationToken cancellationToken = default)
+            where TCommand : CommandBase;
+    }
+}
