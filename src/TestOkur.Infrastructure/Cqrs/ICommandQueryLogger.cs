585c8a83a5c51802c672e8209efa2636e1c51d84
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-02 16:15:47 +0800

    ICommandQueryLogger added

---
+++ src/TestOkur.Infrastructure/Cqrs/ICommandQueryLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace TestOkur.Infrastructure.Cqrs
+{
+    using System.Threading.Tasks;
+
+    public interface ICommandQueryLogger
+    {
+        Task LogAsync<TItem>(TItem item);
+    }
+}
--- src/TestOkur.Infrastructure/Cqrs/Processor.cs
+++ src/TestOkur.Infrastructure/Cqrs/Processor.cs
@@ -1,5 +1,6 @@
 ﻿namespace TestOkur.Infrastructure.Cqrs
 {
+    using Newtonsoft.Json;
     using Paramore.Brighter;
     using Paramore.Darker;
     using System.Threading;
@@ -10,22 +11,27 @@
         private readonly IAmACommandProcessor _commandProcessor;
         private readonly IQueryProcessor _queryProcessor;
         private readonly IUserIdProvider _userIdProvider;
+        private readonly ICommandQueryLogger _commandQueryLogger;
 
-        public Processor(IAmACommandProcessor commandProcessor, IUserIdProvider userIdProvider, IQueryProcessor queryProcessor)
+        public Processor(IAmACommandProcessor commandProcessor, IUserIdProvider userIdProvider, IQueryProcessor queryProcessor, ICommandQueryLogger commandQueryLogger)
         {
             _commandProcessor = commandProcessor;
             _userIdProvider = userIdProvider;
             _queryProcessor = queryProcessor;
+            _commandQueryLogger = commandQueryLogger;
         }
 
         public async Task SendAsync<TCommand>(TCommand command, CancellationToken cancellationToken = default) where TCommand : CommandBase
         {
+            await _commandQueryLogger.LogAsync(JsonConvert.SerializeObject(command));
             command.UserId = await _userIdProvider.GetAsync();
             await _commandProcessor.SendAsync(command, cancellationToken: cancellationToken);
         }
 
         public async Task<TResult> ExecuteAsync<TQuery, TResult>(TQuery query, CancellationToken cancellationToken = default) where TQuery : QueryBase<TResult>
         {
+            await _commandQueryLogger.LogAsync(JsonConvert.SerializeObject(query));
+
             if (query.UserId == default)
             {
                 query.UserId = await _userIdProvider.GetAsync();
