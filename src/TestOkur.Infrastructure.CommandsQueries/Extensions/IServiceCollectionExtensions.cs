106ad3b0995661d3422a606bf3c002a4d6f9e9b5
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-18 21:48:22 +0800

    Dont load dynamic assembly

--- src/TestOkur.Infrastructure.CommandsQueries/Extensions/IServiceCollectionExtensions.cs
+++ src/TestOkur.Infrastructure.CommandsQueries/Extensions/IServiceCollectionExtensions.cs
@@ -6,17 +6,22 @@
     using Paramore.Darker;
     using Paramore.Darker.AspNetCore;
     using System;
+    using System.Linq;
 
     public static class IServiceCollectionExtensions
     {
         public static IServiceCollection AddCommandsAndQueries(this IServiceCollection services)
         {
             services.AddDarker()
-                .AddHandlersFromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                .AddHandlersFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()
+                    .Where(p => !p.IsDynamic)
+                    .ToArray())
                 .AddCustomDecorators();
 
             services.AddBrighter()
-                .AsyncHandlersFromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                .AsyncHandlersFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()
+                    .Where(p => !p.IsDynamic)
+                    .ToArray())
                 .AddPipelineHandlers();
 
             services.Decorate<IAmACommandProcessor, CommandProcessorDecorator>();
