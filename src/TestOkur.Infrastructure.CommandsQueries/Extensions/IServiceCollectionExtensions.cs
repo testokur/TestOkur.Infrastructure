e6b4053b6e9f91e069a5192707a8ffd7429e67ad
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-18 10:50:46 +0800

    DI extension added for commands and queries

---
+++ src/TestOkur.Infrastructure.CommandsQueries/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace TestOkur.Infrastructure.CommandsQueries.Extensions
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Paramore.Brighter.Extensions.DependencyInjection;
+    using Paramore.Darker.AspNetCore;
+    using System;
+    using TestOkur.Infrastructure.CommandsQueries.Commands;
+
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCommandsAndQueries(this IServiceCollection services)
+        {
+            services.AddDarker()
+                .AddHandlersFromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                .AddCustomDecorators();
+
+            services.AddBrighter()
+                .AsyncHandlersFromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                .AddPipelineHandlers();
+
+            return services;
+        }
+    }
+}
