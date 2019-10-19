8394bca2ca2a1fde479ea31aeadd454baedcd09c
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-19 10:00:59 +0800

    HeaderTelemetryInitializer impl for tracing IP address of client

---
+++ src/TestOkur.Infrastructure.Mvc/Monitoring/HeaderTelemetryInitializer.cs
@@ -0,0 +1,34 @@
+﻿namespace TestOkur.Infrastructure.Mvc.Monitoring
+{
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.AspNetCore.Http;
+
+    public class HeaderTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HeaderTelemetryInitializer(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            var context = _httpContextAccessor.HttpContext;
+
+            if (!(telemetry is RequestTelemetry requestTelemetry) || context == null)
+            {
+                return;
+            }
+
+            requestTelemetry.Properties.Add("ClientIp", context?.Connection?.RemoteIpAddress?.ToString());
+
+            foreach (var (key, value) in context.Request.Headers)
+            {
+                requestTelemetry.Properties.Add(key, string.Join(',', value));
+            }
+        }
+    }
+}
