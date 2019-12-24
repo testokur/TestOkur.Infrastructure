e33accc32a361c53b3779656e26bb8dda8ffa863
Nazmi Altun <nazmialtun@windowslive.com>
2019-12-24 16:55:03 +0800

    ReadAllTextAsync impl. and test

---
+++ src/TestOkur.Infrastructure.Mvc/Helpers/FileEx.cs
@@ -0,0 +1,39 @@
+﻿namespace TestOkur.Infrastructure.Mvc.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class FileEx
+    {
+        public static async Task<string> ReadAllTextAsync(string path)
+        {
+            var lines = await File.ReadAllLinesAsync(path, Encoding.UTF8);
+
+            return Concat(lines);
+        }
+
+        private static string Concat(string[] lines)
+        {
+            var s = new string('\0', lines.Sum(l => l.Length + Environment.NewLine.Length));
+            var target = MemoryMarshal.AsBytes(MemoryMarshal.AsMemory(s.AsMemory()).Span);
+            var newLine = MemoryMarshal.AsBytes(MemoryMarshal.AsMemory(Environment.NewLine.AsMemory()).Span);
+
+            for (int i = 0, dest = 0; i < lines.Length; i++)
+            {
+                var lineSource = MemoryMarshal.AsBytes(MemoryMarshal.AsMemory(lines[i].AsMemory()).Span);
+                Unsafe.CopyBlock(ref target[dest], ref lineSource[0], (uint)lineSource.Length);
+                dest += lineSource.Length;
+                Unsafe.CopyBlock(ref target[dest], ref newLine[0], (uint)newLine.Length);
+                dest += newLine.Length;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ tests/TestOkur.Infrastructure.Tests/FileExTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using TestOkur.Infrastructure.Mvc.Helpers;
+using Xunit;
+
+namespace TestOkur.Infrastructure.Tests
+{
+    public class FileExTests
+    {
+        [Fact]
+        public async Task ShouldReadAllText()
+        {
+            const string filePath = "dummy.txt";
+
+            await CreateADummyFileIfNotExists(filePath);
+            var text = await FileEx.ReadAllTextAsync(filePath);
+            text.Should().BeEquivalentTo(File.ReadAllText(filePath));
+        }
+
+        private Task CreateADummyFileIfNotExists(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return Task.CompletedTask;
+            }
+            var lines = new List<string>();
+
+            for (var i = 0; i < 1000; i++)
+            {
+                lines.Add(Guid.NewGuid().ToString("N"));
+            }
+
+            return File.WriteAllLinesAsync(filePath, lines);
+        }
+    }
+}
